3cba140975f6b3ed7dc024cb26afbd77ed3697c7
rong5026 <rong5026@naver.com>
2022-06-28 23:25:51 +0900

    [UPDATE] 테마,버튼 색상변경

--- app/src/main/java/com/example/competition1/MainActivity.java
+++ app/src/main/java/com/example/competition1/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.competition1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(this,"확인1 버튼이 눌렸어요",Toast.LENGTH_LONG).show();
     }
 }
