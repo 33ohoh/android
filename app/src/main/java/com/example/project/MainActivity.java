051662bcbf85c0f896385ef1d1aea4ef65a63d76
ming-taro <hera002008@naver.com>
2022-06-29 01:55:11 +0900

    [ADD] 예측정보를 검색할 때 선택할 조건 목록 추가

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //작물 목록
+        Spinner cropSpinner = (Spinner)findViewById(R.id.spinner_crop);
+        ArrayAdapter cropAdapter = ArrayAdapter.createFromResource(
+                this, R.array.crop, android.R.layout.simple_spinner_item);
+
+        cropAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cropSpinner.setAdapter(cropAdapter);
     }
 }
