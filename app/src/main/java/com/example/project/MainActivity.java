df8ead04881af3aa966756a00e212e55e0868608
ming-taro <hera002008@naver.com>
2022-06-30 04:22:56 +0900

    [ADD] 예측페이지 검색조건 UI 간격, 테두리, 크기 수정

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -14,7 +14,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_page);
+        setContentView(R.layout.activity_main);
 
+        //작물 목록
+        Spinner cropSpinner = (Spinner)findViewById(R.id.spinner_crop);
+        ArrayAdapter cropAdapter = ArrayAdapter.createFromResource(
+                this, R.array.crop, android.R.layout.simple_spinner_item);
+
+        cropAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cropSpinner.setAdapter(cropAdapter);
+
+        FrameLayout predictionResult = (FrameLayout)findViewById(R.id.prediction_result);
+        Button predictionResultInquiry = (Button)findViewById(R.id.btn_prediction_result_inquiry);
+        predictionResultInquiry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                predictionResult.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
