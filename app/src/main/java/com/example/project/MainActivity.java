6cc43aff08d14bf08543cf3f4256be36b1be1356
ming-taro <hera002008@naver.com>
2022-06-29 11:46:39 +0900

    [ADD] 마이페이지 추가

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -14,23 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.my_page);
 
-        //작물 목록
-        Spinner cropSpinner = (Spinner)findViewById(R.id.spinner_crop);
-        ArrayAdapter cropAdapter = ArrayAdapter.createFromResource(
-                this, R.array.crop, android.R.layout.simple_spinner_item);
-
-        cropAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        cropSpinner.setAdapter(cropAdapter);
-
-        FrameLayout predictionResult = (FrameLayout)findViewById(R.id.prediction_result);
-        Button predictionResultInquiry = (Button)findViewById(R.id.btn_prediction_result_inquiry);
-        predictionResultInquiry.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                predictionResult.setVisibility(View.VISIBLE);
-            }
-        });
     }
 }
