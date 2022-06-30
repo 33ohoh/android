83a01a7bb6086753f0e55c0696768388acaca7ab
ming-taro <hera002008@naver.com>
2022-07-01 02:34:45 +0900

    [ADD] 나의 병해충 피해 신고 내역 페이지 추가(제목 검색, 작성버튼)

--- app/src/main/java/com/example/competition1/MainActivity.java
+++ app/src/main/java/com/example/competition1/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.competition1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +18,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_pest_damage_report_history);
+
+        ListView listView = findViewById(R.id.listView);
+
+        List<String> pestDamageReportHistory = new ArrayList<>();
+
+        pestDamageReportHistory.add("사과");
+        pestDamageReportHistory.add("바나나");
+        pestDamageReportHistory.add("키위");
+        pestDamageReportHistory.add("딸기");
+        pestDamageReportHistory.add("수박");
+        pestDamageReportHistory.add("귤");
+        pestDamageReportHistory.add("참외");
+        pestDamageReportHistory.add("딸기");
+        pestDamageReportHistory.add("수박");
+        pestDamageReportHistory.add("귤");
+        pestDamageReportHistory.add("참외");
+        pestDamageReportHistory.add("딸기");
+        pestDamageReportHistory.add("수박");
+        pestDamageReportHistory.add("귤");
+        pestDamageReportHistory.add("참외");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, pestDamageReportHistory);
+
+        listView.setAdapter(adapter);
 
         //MapView mapView = new MapView(this);
 
