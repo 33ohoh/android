095a12bf038fab1876d899ffc92b3da357b25446
rong5026 <rong5026@naver.com>
2022-06-30 21:29:01 +0900

    [UPDTAE] 로그인 UI배치 전부수정

--- app/src/main/java/com/example/competition1/MainActivity.java
+++ app/src/main/java/com/example/competition1/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.competition1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,11 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.test);
 
         //MapView mapView = new MapView(this);
 
         //ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         //mapViewContainer.addView(mapView);
+
+        Button registration;
+        registration = findViewById(R.id.register_button);
+
+        registration.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class );
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
