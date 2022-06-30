b175f32ebfe3901760e051c5c0a299ac7814c1b5
ming-taro <hera002008@naver.com>
2022-06-30 14:46:46 +0900

    [ADD] 마이페이지에서 비밀번호 변경 페이지 추가

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,23 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //작물 목록
-        Spinner cropSpinner = (Spinner)findViewById(R.id.spinner_crop);
-        ArrayAdapter cropAdapter = ArrayAdapter.createFromResource(
-                this, R.array.crop, android.R.layout.simple_spinner_item);
-
-        cropAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        cropSpinner.setAdapter(cropAdapter);
-
-        FrameLayout predictionResult = (FrameLayout)findViewById(R.id.prediction_result);
-        Button predictionResultInquiry = (Button)findViewById(R.id.btn_prediction_result_inquiry);
-        predictionResultInquiry.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                predictionResult.setVisibility(View.VISIBLE);
-            }
-        });
+        setContentView(R.layout.activity_password_reset);
     }
 }
