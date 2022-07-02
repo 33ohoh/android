a4e6c4ea0baf3f957446b9671974aeb59517203d
rong5026 <rong5026@naver.com>
2022-07-02 20:38:31 +0900

    [ADD] 회원가입 페이지에 뒤로가기 툴바 설정

--- app/src/main/java/com/example/competition1/MainActivity.java
+++ app/src/main/java/com/example/competition1/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.competition1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -13,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 1000);
         //MapView mapView = new MapView(this);
 
         //ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
