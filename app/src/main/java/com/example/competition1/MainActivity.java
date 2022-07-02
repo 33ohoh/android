6211d7dc9c129a513886e43cdc9d3a8b5217fdca
rong5026 <rong5026@naver.com>
2022-07-02 13:53:03 +0900

    [UPDATE] 회원가입페이지 레이아웃 수정

--- app/src/main/java/com/example/competition1/MainActivity.java
+++ app/src/main/java/com/example/competition1/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.competition1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,18 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
         //ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         //mapViewContainer.addView(mapView);
-
-        Button registration;
-        registration = findViewById(R.id.register_button);
-
-        registration.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class );
-                startActivity(intent);
-            }
-        });
 
 
     }
