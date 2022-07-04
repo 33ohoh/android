f2bc38d572156da095db835595d4f20383591e9a
ming-taro <hera002008@naver.com>
2022-07-04 17:59:02 +0900

    [ADD] 이번달에 주의해야할 병해충 정보 추가

--- app/src/main/java/com/example/competition1/MainActivity.java
+++ app/src/main/java/com/example/competition1/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.competition1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,7 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_report_history);
+        setContentView(R.layout.activity_sample);
+
+        WebView webView;
+        WebSettings webSettings;
+
+        webView = findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient()); //클릭시 새창 뜨지않게
+        webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
 
         //MapView mapView = new MapView(this);
 
