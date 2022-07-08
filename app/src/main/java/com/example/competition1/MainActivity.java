107d869a5fa91ee56483fe730a856c6ba913b175
rong5026 <rong5026@naver.com>
2022-07-09 04:13:44 +0900

    [ADD] 메뉴바 각 페이지마다 적용

--- app/src/main/java/com/example/competition1/MainActivity.java
+++ app/src/main/java/com/example/competition1/MainActivity.java
@@ -1,35 +1,66 @@
 package com.example.competition1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.competition1.fragment.FragmentCurrent;
+import com.example.competition1.fragment.FragmentDeclaration;
+import com.example.competition1.fragment.FragmentInformation;
+import com.example.competition1.fragment.FragmentMypage;
+import com.example.competition1.fragment.FragmentPredict;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Handler handler = new Handler();
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentCurrent fragmentCurrent = new FragmentCurrent();
+    private FragmentDeclaration fragmentDeclaration = new FragmentDeclaration();
+    private FragmentInformation fragmentInformation = new FragmentInformation();
+    private FragmentMypage fragmentMypage = new FragmentMypage();
+    private FragmentPredict fragmentPredict = new FragmentPredict();
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentPredict).commitAllowingStateLoss();
 
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        }, 1000);
-
-
-
-
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
     }
 
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.tab_current:
+                    transaction.replace(R.id.frameLayout, fragmentCurrent).commitAllowingStateLoss();
+                    break;
+                case R.id.tab_declaration:
+                    transaction.replace(R.id.frameLayout, fragmentDeclaration).commitAllowingStateLoss();
+                    break;
+                case R.id.tab_information:
+                    transaction.replace(R.id.frameLayout, fragmentInformation).commitAllowingStateLoss();
+                    break;
+                case R.id.tab_mypage:
+                    transaction.replace(R.id.frameLayout, fragmentMypage).commitAllowingStateLoss();
+                    break;
+                case R.id.predict_menu:
+                    transaction.replace(R.id.frameLayout, fragmentPredict).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
+    }
 }
---
+++ app/src/main/java/com/example/competition1/fragment/FragmentPredict.java
@@ -0,0 +1,22 @@
+package com.example.competition1.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.competition1.R;
+
+
+public class FragmentPredict extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_predict, container, false);
+    }
+}
