8795192f0228578cbdbe7c70c0ac8cfaa518aa45
ming-taro <hera002008@naver.com>
2022-07-10 18:38:31 +0900

    [ADD] 서버에서 신고내역 데이터 연동, 나의 신고내역에서 확인 가능하도록 연결함

--- app/src/main/java/com/example/competition1/LoginActivity.java
+++ app/src/main/java/com/example/competition1/LoginActivity.java
@@ -63,9 +63,7 @@ public class LoginActivity extends Activity implements View.OnClickListener {
             case R.id.login_button:
                 id = editId.getText().toString();
                 password =editPassword.getText().toString();
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                //requestLogin(id,password );
+                requestLogin(id,password );
                 break;
 
             case R.id.finding_button:
