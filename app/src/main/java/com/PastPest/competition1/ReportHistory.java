72701ce8afc1e689231a0d1b8b073da47b9c0e1f
hong <rong5026@naver.com>
2022-08-07 15:34:45 +0900

    [UPDATE] 해결완료된 신고내역 표시안하기

--- app/src/main/java/com/PastPest/competition1/ReportHistory.java
+++ app/src/main/java/com/PastPest/competition1/ReportHistory.java
@@ -15,6 +15,7 @@ public class ReportHistory implements Serializable {
     private String details;
     private String title;
     private String date;
+    private String isSolved;
 
 
     public ReportHistory(String title, String date, String address, String cropName,
@@ -31,7 +32,7 @@ public class ReportHistory implements Serializable {
 
 
 
-    public ReportHistory(String cropName, String details, Double latitude, Double longitude,String id, String address,String title, String date,String symptom,String pestName){
+    public ReportHistory(String cropName, String details, Double latitude, Double longitude,String id, String address,String title, String date,String symptom,String pestName,String isSolved){
         this.cropName = cropName;
         this.details = details;
         this.latitude = latitude;
@@ -42,6 +43,7 @@ public class ReportHistory implements Serializable {
         this.date = date;
         this.symptom =symptom;
         this.pestName= pestName;
+        this.isSolved =isSolved;
     }
 
     public ReportHistory(String imageUrl, String cropName, String details,String id, String address,String title, String date,String symptom ,String pestName){
@@ -55,6 +57,8 @@ public class ReportHistory implements Serializable {
         this.symptom =symptom;
         this.pestName = pestName;
     }
+
+    public String getIsSolved(){return isSolved;}
 
     public String getId() {
         return id;
